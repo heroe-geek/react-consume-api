4b22b9a34ea109dd222fb16458df2abbe8cdb9c8
heroe-geek <heroes.geek2020@gmail.com>
2020-06-24 14:47:42 -0500

    Deploy REACT GH-PAGES

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export class ProductService {
 
     // baseUrl = "http://localhost:8080/api/products/";
-    baseUrl = "http://hg-rest-api.herokuapp.com/api/products/";
+    baseUrl = "https://hg-rest-api.herokuapp.com/api/products/";
 
     create(product){
         return axios.post(this.baseUrl+"product/", product).then(res => res.data);
